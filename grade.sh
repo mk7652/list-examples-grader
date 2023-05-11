7a6db562a5389620390bc3730eb4a30f8a0e8321
mk7652 <130100151+mk7652@users.noreply.github.com>
2023-05-11 13:44:43 -0700

    hi

--- grade.sh
+++ grade.sh
@@ -8,6 +8,20 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -e student-submission ]]
+then 
+    cp student-submission/ListExamples.java grading-area 
+    cp TestListExamples.java grading-area
+    cd grading-area
+    javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
+    if [[ $? == 1 ]]
+    then 
+        echo "compile error"
+        exit 1
+    fi
+    java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+fi
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
